68ec5379dd1ecfd55308e3f6e4d4325bdc8f1130
José Antonio Canalo González <joseantoniocgonzalez83@gmail.com>
2026-01-29 11:59:49 +0100

    Refactor hello endpoint to service and keep consistent JSON errors

--- spring-boot-api/src/main/java/org/jose/learning/api/controller/HelloController.java
+++ spring-boot-api/src/main/java/org/jose/learning/api/controller/HelloController.java
@@ -1,12 +1,12 @@
 package org.jose.learning.api.controller;
 
-import org.jose.learning.api.dto.ErrorResponse;
 import org.jose.learning.api.dto.HelloResponse;
 import org.jose.learning.api.service.HelloService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class HelloController {
@@ -18,13 +18,13 @@ public class HelloController {
     }
 
     @GetMapping("/api/hello")
-    public ResponseEntity<?> hello(@RequestParam(required = false) String name) {
+    public HelloResponse hello(@RequestParam(required = false) String name) {
         if (name == null) {
-            return ResponseEntity.ok(helloService.getHello());
+            return helloService.getHello();
         }
         if (name.isBlank()) {
-            return ResponseEntity.badRequest().body(new ErrorResponse("name must not be blank"));
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "name must not be blank");
         }
-        return ResponseEntity.ok(helloService.getHelloForName(name));
+        return helloService.getHelloForName(name);
     }
 }
